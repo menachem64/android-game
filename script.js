dc08209736decbd7c2cb3e45549b701796ee7555
menachem64 <95292158+menachem64@users.noreply.github.com>
2023-12-31 15:56:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -89,7 +89,10 @@ const adversaryForPush = {
     ],
     y:[ 
         35,
-   
+        200,
+        365,
+        119.5,   
+        //  282.5,
     ],
     w:[
         80,
@@ -185,12 +188,12 @@ function biting(){
         for (let i = 0; i < bite.w.length; i++) {
         bite.w[i] = 0;
         biteForPush.w[i] = 0;
-}}, 2000)
+}}, 4000)
     setInterval(() => {
         for (let i = 0; i < bite.w.length; i++) {
         bite.w[i] = 23.2;
         biteForPush.w[i] = 23.2;
-}}, 1000)
+}}, 2000)
 }
 
 biting();
